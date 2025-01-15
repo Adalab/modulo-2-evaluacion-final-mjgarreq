4cb0abf20d56608a95e66ca5c97369a390208213
María José <majosegarcia96@gmail.com>
2025-01-15 09:50:27 +0100

    basic structure HTML

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('>> Ready :)');
+ /*
+ <li>
+          <article>
+            <img src="https://placehold.co/600x400?text=Hello+World" alt="">
+            <h3>naruto</h3>
+          </article>
+        </li>
+ */
